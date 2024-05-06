dcff18e7134a9c06780f60493fee14dafd567975
Brady Anderson <brady@bradyanderson.tech>
2024-05-05 20:44:54 -0600

    first-tilemap gb

---
+++ graphics/first-tilemap/main.asm
@@ -0,0 +1,59 @@
+INCLUDE "hardware.inc"
+
+SECTION "Header", ROM0[$100]
+
+    jp EntryPoint
+
+    ds $150 - @, 0 ; Make room for the header
+
+EntryPoint:
+    ; Shut down audio circuitry
+    ld a, 0
+    ld [rNR52], a
+
+    ; Wait for VBlank to turn the LCD off safely
+WaitVBlank:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank
+
+    ; Turn the LCD off
+    ld a, 0
+    ld [rLCDC], a
+
+    ; Copy the tile data
+    ld de, Tiles
+    ld hl, $9000 ; Starting address in VRAM for tile data
+    ld bc, 32 ; 16 bytes per tile; load 2 tiles this time
+CopyTile:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTile
+
+	; Copy the tilemap
+	ld de, Tilemap
+	ld hl, $9800
+	ld bc, TilemapEnd - Tilemap
+CopyTilemap:
+	ld a, [de]
+	ld [hli], a
+	inc de
+	dec bc
+	ld a, b
+	or a, c
+	jp nz, CopyTilemap
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON
+    ld [rLCDC], a
+
+    ; Set background palette
+    ld a, %11100100
+    ld [rBGP], a
+
+Done:
+    jp Done
---
+++ graphics/first-tilemap/tiles.asm
@@ -0,0 +1,7 @@
+INCLUDE "hardware.inc"
+
+SECTION "Tile data", ROM0
+
+Tiles::
+  db $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+  db $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
