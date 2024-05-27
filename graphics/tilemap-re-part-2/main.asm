e7b2d6b78d172badada3030f66ab6f0faa7df0c0
Brady Anderson <bradya48@gmail.com>
2024-05-27 15:32:09 -0600

    Start tilemap reverse engineering part 2, create 2bpp from make

---
+++ graphics/tilemap-re-part-2/main.asm
@@ -0,0 +1,50 @@
+INCLUDE "hardware.inc"
+
+SECTION "Header", ROM0[$100]
+
+    jp EntryPoint
+
+    ds $150 - @, 0 ; Make room for the header
+
+EntryPoint:
+    ; Shut down audio circuitry
+    ld a, 0
+    ld [rNR52], a
+
+    ; Wait for VBlank to turn the LCD off safely
+WaitVBlank:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank
+
+    ; Turn the LCD off
+    ld a, 0
+    ld [rLCDC], a
+
+    ; Copy the tile data
+    ld de, BackgroundTileData
+    ld hl, $9000 ; Starting address in VRAM for tile data
+    ld bc, BackgroundTileDataEnd - BackgroundTileData
+CopyTiles:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTiles
+
+    ; Turn the LCD on
+    ld a, LCDCF_ON | LCDCF_BGON
+    ld [rLCDC], a
+
+    ; Set background palette
+    ld a, %11100100
+    ld [rBGP], a
+
+Done:
+    jp Done
+
+; Include the background tile data
+BackgroundTileData: INCBIN "background.2bpp"
+BackgroundTileDataEnd:
